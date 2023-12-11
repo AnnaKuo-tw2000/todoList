381ec49f0ad968c5276e7d063a89896b4bb8fc1c
Anna Kuo <juice1114guo@gmail.com>
2023-12-11 10:51:20 +0800

    新增：按鈕呈現完成、未完成事項

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,63 @@
+<script setup>
+import { useCounterStore } from '../stores/counter';
+
+const store = useCounterStore();
+
+</script>
+<template>
+    <footer class="todoList__footer bg-primary dark:bg-dkPrimary" :class="{ dark: isDark }">
+        <el-button plain round class="todoList__filterBtn" :class="{ active: store.filterStatus === 'all' }"
+            @click="store.filterStatus = 'all'">All</el-button>
+        <el-button plain round class="todoList__filterBtn" :class="{ active: store.filterStatus === 'completed' }"
+            @click="store.filterStatus = 'completed'">Completed</el-button>
+        <el-button plain round class="todoList__filterBtn" :class="{ active: store.filterStatus === 'incomplete' }"
+            @click="store.filterStatus = 'incomplete'">InComplete</el-button>
+    </footer>
+</template>
+<style lang="scss" scoped>
+.todoList__footer {
+    @apply flex justify-evenly items-start py-4;
+    position: relative;
+    z-index: 0;
+
+    .todoList__filterBtn {
+        &.el-button {
+            position: relative;
+            border: none;
+
+            &:before {
+                content: '';
+                width: 100%;
+                height: 0;
+                position: absolute;
+                bottom: 50%;
+                z-index: -1;
+                background-color: white;
+                transition: height .3s;
+            }
+
+            &.active {
+                @apply text-primary;
+
+                &::before {
+                    height: 32px;
+                }
+            }
+        }
+    }
+
+    &.dark {
+        .todoList__filterBtn {
+            &.el-button {
+                &:before {
+                    @apply bg-dkSecondary;
+                }
+
+                &.active {
+                    @apply text-dkTertiary bg-dkSecondary;
+                }
+            }
+        }
+    }
+}
+</style>
