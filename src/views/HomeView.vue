3abab1916487afdc7cf1ee71977deb5f933972b6
Anna Kuo <juice1114guo@gmail.com>
2023-12-13 10:37:06 +0800

    修改：main區塊高度

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,11 @@
 <script setup>
+// main區塊高度計算
+onMounted(() => {
+    const headerHeight = document.querySelector('header.todoList__header')?.offsetHeight || 0;
+    const footerHeight = document.querySelector('footer.todoList__footer')?.offsetHeight || 0;
+    const mainEle = document.querySelector('main.todoList__main');
+    mainEle.style.height = `calc(var(--vh, 1vh) * 100 - ${headerHeight}px - ${footerHeight}px)`;
+});
 </script>
 
 <template>
